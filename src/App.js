1ac3d0f5f65882c401d6f18d1f474f78d220204a
Cai Yang <caih.yang@gmail.com>
2019-05-13 02:29:06 -0400

    adding more components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 import aniManage from './images/ani-manage';
 import arcade from './images/arcade.png';
 import league from './images/league.png';
+import calc from './images/calc.png';
+import tic from './images/tic.png';
 import {Icon,Card,List,Button,Reveal,Modal} from 'semantic-ui-react'
+import ModalCard from './components/ModalCard'
+
 class App extends Component {
 
   componentDidMount() {
@@ -105,20 +109,9 @@ class App extends Component {
                   </div>
                   <Card.Header className="cardTitles">Ani-Manage</Card.Header>
 
-                  <Reveal animated='move'>
-                    <Reveal.Content visible>
-                      <Card.Description className="cardDescrip">
-                        An animation search engine built on React.js that uses data pulled from Jikan's API.
-                        Managing states in a way that allows users to bookmark favorites as well as the integration of local storage
-                        to store bookmark data. Deployed with Heroku.
-                      </Card.Description>
-                    </Reveal.Content>
-                    <Reveal.Content hidden>
-                      <Modal trigger={<img className="work-pics" src={aniManage}></img>}>
-                        <Modal.Content><img className="work-pics" src={aniManage}></img></Modal.Content>
-                      </Modal>
-                    </Reveal.Content>
-                  </Reveal>
+                  <ModalCard descrip="An animation search engine built on React.js that uses data pulled from Jikan's API.
+                    Managing states in a way that allows users to bookmark favorites as well as the integration of local storage
+                    to store bookmark data. Deployed with Heroku." img={aniManage}/>
 
                 </Card.Content>
                 <Card.Content extra>
@@ -141,19 +134,8 @@ class App extends Component {
                   </div>
                   <Card.Header className="cardTitles">Cai's Arcade</Card.Header>
 
-                  <Reveal animated='move'>
-                    <Reveal.Content visible>
-                      <Card.Description className="cardDescrip">
-                        A fully functional web based arcade filled with games based of remakes of the classics or creation on the spot.
-                        Uses HTML/CSS and a lot OOP Javascript.
-                      </Card.Description>
-                    </Reveal.Content>
-                    <Reveal.Content hidden>
-                      <Modal trigger={<img className="work-pics" src={arcade}></img>}>
-                        <Modal.Content><img className="work-pics" src={arcade}></img></Modal.Content>
-                      </Modal>
-                    </Reveal.Content>
-                  </Reveal>
+                  <ModalCard descrip="A fully functional web based arcade filled with games based of remakes of the classics or creation on the spot.
+                    Uses HTML/CSS and a lot OOP Javascript." img={arcade} />
 
                 </Card.Content>
                 <Card.Content extra>
@@ -173,19 +155,7 @@ class App extends Component {
                     <a href="https://my-league-picks.netlify.com/" target="_blank" rel="noopener noreferrer"><Icon color="black" size='big' name="globe"></Icon></a>
                   </div>
                   <Card.Header className="cardTitles">League-Picks</Card.Header>
-
-                  <Reveal animated='move'>
-                    <Reveal.Content visible>
-                      <Card.Description className="cardDescrip">
-                        Blog like webpage to show mastery in HTML, CSS and Javascript. Implemented best practices in the Web Developement field.
-                      </Card.Description>
-                    </Reveal.Content>
-                    <Reveal.Content hidden>
-                      <Modal trigger={<img className="work-pics" src={league}></img>}>
-                        <Modal.Content><img className="work-pics" src={league}></img></Modal.Content>
-                      </Modal>
-                    </Reveal.Content>
-                  </Reveal>
+                  <ModalCard descrip="Blog like webpage to show mastery in HTML, CSS and Javascript. Implemented best practices in the Web Developement field." img={league} />
 
                 </Card.Content>
                 <Card.Content extra>
@@ -205,9 +175,9 @@ class App extends Component {
                     <a href="https://web-calculator.netlify.com/" target="_blank" rel="noopener noreferrer" ><Icon color="black" size='big' name="globe"></Icon></a>
                   </div>
                   <Card.Header className="cardTitles">Web-Calculator</Card.Header>
-                  <Card.Description className="cardDescrip">
-                    Interactive web calculator that does all basic operations with an addition of an average feature.
-                  </Card.Description>
+
+                  <ModalCard descrip="Interactive web calculator that does all basic operations with an addition of an average feature." img={calc}/>
+
                 </Card.Content>
                 <Card.Content extra>
                   <List horizontal>
@@ -225,9 +195,9 @@ class App extends Component {
                     <a href="https://tic-tac-toe-cy.netlify.com/" target="_blank" rel="noopener noreferrer" ><Icon color="black" size='big' name="globe"></Icon></a>
                   </div>
                   <Card.Header className="cardTitles">Tic-Tac-Toe</Card.Header>
-                  <Card.Description className="cardDescrip">
-                    Simple game of the classic Tic-Tac-Toe using Javascript while interacting with the DOM.
-                  </Card.Description>
+
+                  <ModalCard descrip="Simple game of the classic Tic-Tac-Toe using Javascript while interacting with the DOM." img={tic}/>
+
                 </Card.Content>
                 <Card.Content extra>
                   <List horizontal>
