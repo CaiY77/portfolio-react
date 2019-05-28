f2475222d630020f2fe56f95d0454b8ffa5cb11d
Cai Yang <caih.yang@gmail.com>
2019-05-28 16:35:28 -0400

    background adjust

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,17 +8,17 @@ class NavBar extends Component {
         <a href="#top" class="item header">
           <h1 className="my-name">CY</h1>
         </a>
-        <div class="right menu r-m">
+        <div class="right menu">
           <a href="#about" class="item nav-item">
             ABOUT
           </a>
-          <a href="#experience" class="item nav-item">
+          <a href="#experience" class="item nav-item r-m">
             EXPERIENCE
           </a>
-          <a href="#works" class="item nav-item">
+          <a href="#works" class="item nav-item r-m">
             WORKS
           </a>
-          <a href="#contact" class="item nav-item">
+          <a href="#contact" class="item nav-item r-m">
             CONTACT
           </a>
         </div>
