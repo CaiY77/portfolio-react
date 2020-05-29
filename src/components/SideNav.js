4800fa15e6587936f666159c201b520a50d6a898
Cai Yang <33525692+CaiY77@users.noreply.github.com>
2020-05-28 21:00:26 -0400

    new sidebar

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react'
-import {Icon} from 'semantic-ui-react'
+import {Button} from 'semantic-ui-react'
 
 class SideNav extends Component {
 
+  constructor(props){
+    super(props);
+
+    this.state=({
+      isOpen: false
+    })
+  }
+
+  toggleOption = () => {
+    let element = document.getElementById('etc')
+    element.classList.toggle('show')
+    
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+    
+    
+  }
+
   render() {
-    return (
-      <div class="ui small compact vertical labeled icon menu vert" style={{'display':'none'}}>
-        <a href="https://www.docdroid.net/xqnHf2p/resume-pdf" rel="noopener noreferrer" target="_blank" class="item">
-          <Icon size="small" name="file alternate"/>
-          Resume
-        </a>
-        <a href="https://www.linkedin.com/in/caihyang/" rel="noopener noreferrer" target="_blank" class="item">
-          <Icon size="small" name="linkedin"/>
-          Linkedin
-        </a>
-        <a href="https://github.com/CaiY77" rel="noopener noreferrer" target="_blank" class="item">
-          <Icon size="small" name="github"/>
-          GitHub
-        </a>
+    const {isOpen} = this.state
+
+    return (<div>
+      {
+        (isOpen)
+        ?<Button className="button-menu op-but-open" icon='close' onClick={()=>this.toggleOption()}></Button>
+        :<Button className="button-menu op-but" icon='options' onClick={()=>this.toggleOption()} ></Button>
+      }
+     <div id="etc" className="hide">
+        <a href="https://www.docdroid.net/xqnHf2p/resume-pdf" rel="noopener noreferrer" target="_blank"><Button className="button-menu res-but" icon="file alternate"></Button></a>
+        <a href="https://www.linkedin.com/in/caihyang/" rel="noopener noreferrer" target="_blank"><Button className="button-menu link-but" icon="linkedin"></Button></a>
+        <a href="https://github.com/CaiY77" rel="noopener noreferrer" target="_blank"><Button className="button-menu git-but" icon="github"></Button></a>
       </div>
-    );
+
+    </div>);
   }
 
 }
