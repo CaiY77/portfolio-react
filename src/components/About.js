808e23ad6e540a619502bda32d7e454d306a5bad
Cai Yang <33525692+CaiY77@users.noreply.github.com>
2020-07-16 12:48:10 -0400

    typo

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ class About extends Component {
             <h1 id="about-n">Cai H. Yang</h1>
             <h2 id="about-h">Software Engineer | Full-Stack Engineer </h2>
             <br/>
-            <p id="about-p">I am enjoy building applications and websites from the ground-up
+            <p id="about-p">I enjoy building applications and websites from the ground-up
             <br/>as well as using logical deduction with algorithmic
             <br/>approaches to solve problems, puzzles and technical issues.</p>
             
